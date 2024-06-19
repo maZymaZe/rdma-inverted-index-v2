219265a8a2f996231532c9df9e898e00741503e5
maze <782618517@qq.com>
2024-06-19 14:04:08 +0000

    fix sh

--- myscript/nfsclient.sh
+++ myscript/nfsclient.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install -y --force-yes nfs-common
-sudo mkdir /mnt/clientfolder/
+sudo mkdir -p /mnt/clientfolder/
 sudo mount 10.10.1.1:/mnt/sharefolder /mnt/clientfolder/
--- myscript/nfsserver.sh
+++ myscript/nfsserver.sh
@@ -1,10 +1,14 @@
 sudo apt update
-sudo apt install -y --force-yes nfs-kernel-server
+sudo apt install -y --force-yes nfs-kernel-server ufw
+sudo ufw allow from 10.10.1.1/24 to any port nfs
+sudo ufw allow ssh
+echo y | sudo ufw enable
 sudo systemctl start nfs-kernel-server.service
 sudo mkdir -p /mnt/sharefolder
 sudo chown nobody:nogroup /mnt/sharefolder/
 sudo chmod -R 777 /mnt/sharefolder/
 echo "\n\n/mnt/sharefolder *(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
 sudo exportfs -arv
+sudo touch /mnt/sharefolder/nfsok
 
 
