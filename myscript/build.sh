2122485ace8df34cfd4771c86d1da275c6b312bf
maze <782618517@qq.com>
2024-06-20 03:31:38 +0000

    fix sh

--- myscript/build.sh
+++ myscript/build.sh
@@ -1,4 +1,4 @@
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_CXX_COMPILER=clang++-12 ..
+mkdir -p ~/rdma-inverted-index-v2/build
+cd ~/rdma-inverted-index-v2/build
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_CXX_COMPILER=clang++-12 ~/rdma-inverted-index-v2
 make
