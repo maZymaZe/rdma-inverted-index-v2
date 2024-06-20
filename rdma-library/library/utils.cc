1ba98580a12ac8b2c64e8e8801e1bf2d9c0e75a8
maze <782618517@qq.com>
2024-06-20 04:34:12 +0000

    modify node

--- rdma-library/library/utils.cc
+++ rdma-library/library/utils.cc
@@ -9,16 +9,16 @@ void lib_failure(const str&& message) {
 
 std::string get_ip(const str& node_name) {
   std::map<str, str> node_to_ip{
-    {"cluster1", "10.10.5.20"},
-    {"cluster2", "10.10.5.21"},
-    {"cluster3", "10.10.5.22"},
-    {"cluster4", "10.10.5.23"},
-    {"cluster5", "10.10.5.24"},
-    {"cluster6", "10.10.5.25"},
-    {"cluster7", "10.10.5.26"},
-    {"cluster8", "10.10.5.27"},
-    {"cluster9", "10.10.5.28"},
-    {"cluster10", "10.10.5.29"},
+    {"cluster1", "10.10.1.1"},
+    {"cluster2", "10.10.1.2"},
+    {"cluster3", "10.10.1.3"},
+    // {"cluster4", "10.10.5.23"},
+    // {"cluster5", "10.10.5.24"},
+    // {"cluster6", "10.10.5.25"},
+    // {"cluster7", "10.10.5.26"},
+    // {"cluster8", "10.10.5.27"},
+    // {"cluster9", "10.10.5.28"},
+    // {"cluster10", "10.10.5.29"},
   };
 
   lib_assert(node_to_ip.find(node_name) != node_to_ip.end(),
